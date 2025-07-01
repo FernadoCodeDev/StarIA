be9b9fc87053205fa33d02f9af43900ce1c7c0aa
Fernando <fernando.e.mejia.e@gmail.com>
2025-07-01 13:00:42 -0600

    Cyan Color

--- src/components/ui/DropDown.jsx
+++ src/components/ui/DropDown.jsx
@@ -16,8 +16,8 @@ const ChatActions = () => {
 
       {open && (
         <ul className="absolute z-10 w-40 mt-2 bg-white border rounded shadow-lg dark:bg-neutral-800">
-          <li className="flex items-center gap-2 p-2 cursor-pointer hover:bg-blue-100 hover:text-blue-600">
-            <File className="w-4 h-4 text-blue-600" /> Archivar
+          <li className="flex items-center gap-2 p-2 cursor-pointer hover:bg-cyan-100 hover:text-cyan-600">
+            <File className="w-4 h-4 text-cyan-600" /> Archivar
           </li>
           <li className="flex items-center gap-2 p-2 cursor-pointer hover:bg-red-100 hover:text-red-600">
             <Delete className="w-4 h-4 text-red-600" /> Eliminar
