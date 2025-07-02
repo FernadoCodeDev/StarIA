0b8bccc1e41254b49c7c56b03c914b405bfe9146
Fernando <fernando.e.mejia.e@gmail.com>
2025-07-02 10:02:59 -0600

    Hourglass

---
+++ src/components/ui/Hourglass.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Hourglass = () => {
+  return (
+    <div className="flex items-center justify-center p-2 m-auto rounded-full w-28 h-28 bg-slate-100">
+      <div class="loader"></div>
+    </div>
+  );
+};
+
+export default Hourglass;
